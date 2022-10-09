e247b92c7b45f2d444a97bd6f1b8fae6a4d438a3
PateRom <romalpesh@gmail.com>
2022-10-09 16:27:15 +0100

    committing the project first time

---
+++ src/test/java/uk/axone/devintest/maven/test/TestApplication.java
@@ -0,0 +1,26 @@
+package uk.axone.devintest.maven.test;
+
+import org.junit.jupiter.api.Test;
+
+
+public class TestApplication {
+    @Test
+    public void test1(){
+        System.out.println("running test 1");
+    }
+    @Test
+    public void test2(){
+
+        System.out.println("running test 2");
+    }
+    @Test
+    public void test3(){
+
+        System.out.println("running test 3");
+    }
+    @Test
+    public void test4(){
+
+        System.out.println("running test 4");
+    }
+}
